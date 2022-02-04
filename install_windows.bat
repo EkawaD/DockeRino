949929dea8712e842f51df0b6ae2613bd95e4d4c
EkawaD <steo.ederhy@gmail.com>
2022-02-04 17:06:14 +0100

    install bat

---
+++ install_windows.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+set current=%cd%
+set install_dir=%USERPROFILE%\.rino\
+IF EXIST %install_dir% (
+    ECHO Previous Dockerino installation detected
+    RMDIR /S /Q %install_dir%
+)
+MKDIR %install_dir%
+CD %install_dir% && git clone https://github.com/EkawaD/DockeRino.git .
+IF EXIST %install_dir% SET PATH=%PATH%;%install_dir%
+ECHO Rino has been added to your $PATH variable
+CD %current%
+
--- rino.bat
+++ rino.bat
@@ -3,14 +3,71 @@
 set app=%1
 set project_name=%2
 set list=(xampp symfony python django flask react vue)
+set current=%cd%
 
-CALL :checkAvailableApp %app%
+tasklist /fi "ImageName eq Docker Desktop.exe" /fo csv 2>NUL | find /I "Docker Desktop.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    set docker_running=true
+) else (
+    set docker_running=false
+)
+
+IF %docker_running%==false (
+ "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+)
+
+IF %app% == run (
+    docker-compose up -d  
+    START http://127.0.0.1:80/www
+) ELSE (
+    CALL :checkAvailableApp %app%
+)
+
 GOTO :EOF
+
+
+:xampp
+CALL :MOVE_TO_DESKTOP %app% %project_name%
+ECHO Starting the docker-compose file...
+CD %USERPROFILE%\Desktop\%~2\
+docker-compose up -d
+START http://127.0.0.1:80/www
+ECHO Web server is UP ! A localhost page should have started.
+ECHO You should read the README.md inside your folder project
+CD %current%
+GOTO :EOF
+
+:symfony
+CALL :MOVE_TO_DESKTOP %app% %project_name%
+CD %USERPROFILE%\Desktop\%~2
+ECHO #.ENV > .env
+ECHO PROJECT=%~2 >> .env
+ECHO PROJECT_URL= >> .env
+ECHO DATABASE_ROOT_PASSWORD=root >> .env
+ECHO DATABASE_NAME=test >> .env
+ECHO MYSQL_USER=test >> .env
+ECHO MYSQL_PASSWORD=test >> .env
+ECHO DATABASE_URL="mysql://root:${DATABASE_ROOT_PASSWORD}@mysql:3306/${DATABASE_NAME}"  >> .env
+ECHO Finished !
+ECHO Starting the docker-compose file...
+docker-compose build
+docker-compose up -d 
+docker exec %~2_symfony composer create-project symfony/website-skeleton app -n
+CD %USERPROFILE%\Desktop\%~2\app
+DEL docker-compose.override.yml
+DEL docker-compose.yml
+CD %USERPROFILE%\Desktop\%~2
+type .env >> app/.env
+START http://127.0.0.1:80
+CD %current%
+GOTO :EOF
+
 
 
 :checkAvailableApp
 FOR %%G IN %list% DO ( 
     IF /I "%~1"=="%%~G" (
+        CD %dockerino_path%
         GOTO MATCH %%~G
     ) 
 )
@@ -35,46 +92,9 @@ FOR %%G IN %list% DO (
 )
 GOTO :EOF
 
-
-
-:xampp
-ECHO Creating %~2 project on your Desktop...
-XCOPY "./.docker/%~1" "%USERPROFILE%\Desktop\%~2\" /s/h/e/k/f/c /Q 
-ECHO Finished !
-
-ECHO Starting the docker-compose file...
-CD %USERPROFILE%\Desktop\%~2\
-docker-compose up -d
-START http://127.0.0.1:80/www
-ECHO Web server is UP ! A localhost page should have started.
-ECHO You should read the README.md inside your folder project
-GOTO :EOF
-
-
-
-:symfony
+:MOVE_TO_DESKTOP
 ECHO Creating %~2 project on your Desktop...
 XCOPY "./.docker/%~1" "%USERPROFILE%\Desktop\%~2\" /s/h/e/k/f/c /Q 
 CD %USERPROFILE%\Desktop\%~2
-ECHO #.ENV > .env
-ECHO PROJECT=%~2 >> .env
-ECHO PROJECT_URL= >> .env
-ECHO DATABASE_ROOT_PASSWORD=root >> .env
-ECHO DATABASE_NAME=test >> .env
-ECHO MYSQL_USER=test >> .env
-ECHO MYSQL_PASSWORD=test >> .env
-ECHO DATABASE_URL="mysql://root:${DATABASE_ROOT_PASSWORD}@mysql:3306/${DATABASE_NAME}"  >> .env
-ECHO Finished !
-
-ECHO Starting the docker-compose file...
-docker-compose build
-docker-compose up -d 
-docker exec %~2_symfony composer create-project symfony/website-skeleton app -n
-CD %USERPROFILE%\Desktop\%~2\app
-DEL docker-compose.override.yml
-DEL docker-compose.yml
-CD %USERPROFILE%\Desktop\%~2
-type .env >> app/.env
-START http://127.0.0.1:80
-
 GOTO :EOF
+
