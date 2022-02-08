e34e16fa915cf7ada5e82587afcf1c2bcf4909b3
EkawaD <steo.ederhy@gmail.com>
2022-02-08 16:10:41 +0100

    refactor delayedExpansion

--- rino.bat
+++ rino.bat
@@ -1,46 +1,50 @@
 @echo Off
+setlocal EnableDelayedExpansion
+
+
 set current=%cd%
-set _batch_dir=%USERPROFILE%/.rino/batch
-call %_batch_dir%/SetupEnv.bat
+set batch_dir=%USERPROFILE%/.rino/batch
+call !batch_dir!/SetupEnv.bat
+
 
 
 set param1=%1
 set param2=%2
 
-if %param1%==start (
-    call %lib% start_docker
+if !param1!==start (
+    call !lib! start_docker
     docker-compose run -d
-) else if %param1%==run (
+) else if !param1!==run (
     call :get_params
     call :is_container_started
-    if %process%==python (
-        docker exec -ti %container% python %param2%
+    if !process!==python (
+        docker exec -ti !container! python !param2!
     ) else (
         echo This project is not a python project !
     )
-) else if  %param1%==update (
-    call %lib% update
-) else if  %param1%==help (
-    call %lib% help 
+) else if  !param1!==update (
+    call !lib! update
+) else if  !param1!==help (
+    call !lib! help 
 ) else (
-    set _app=%param1%
-    set _project_name=%param2%
-    call :get_app %_app%
+    set _app=!param1!
+    set _project_name=!param2!
+    call :get_app !_app!
 )
 
 
-call %_batch_dir%/CleanEnv.bat
+call !_batch_dir!/CleanEnv.bat
 
 goto :eof
 
 
 :get_params
-for %%* in (.) do set _project=%%~nx*
+for %%* in (.) do set project=%%~nx*
 for /f "tokens=*" %%i in ('type .env') do (
-    set _line=%%i
-    for /F "tokens=2 delims==" %%a in ("%_line%") do ( 
-        set _process=%%a
-        set _container=%_project%_%_process%
+    set line=%%i
+    for /F "tokens=2 delims==" %%a in ("!line!") do ( 
+        set process=%%a
+        set container=!project!_!process!
     )
     goto :eof
 )
@@ -48,14 +52,14 @@ goto :eof
     
 :is_container_started
 for /F "tokens=*" %%i in ('docker ps --format {{.Names}}') do (
-    if not %%i==%_container% (
+    if not %%i==!container! (
         echo No container for this project is currently running, you should use [rino start] first
     ) 
 )
 goto :eof
 
 :get_app
-for %%G in %list% do ( 
+for %%G in !list! do ( 
     if /I "%~1"=="%%~G" (
         goto :match %%~G
     ) 
@@ -64,11 +68,11 @@ goto :eof
 
 :match
 if %~1==xampp (
-    call %lib% xampp %_app% %_project_name%
+    call !lib! xampp !_app! !_project_name!
 ) else if %~1==symfony (
-    call %lib% symfony %_app% %_project_name%
+    call !lib! symfony !_app! !_project_name!
 ) else if %~1==python (
-    call %lib% python %_app% %_project_name%
+    call !lib! python !_app! !_project_name!
 ) else (
     echo Erreur !
 )
