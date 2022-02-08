d922b527b754d4ba8f670c24bc8ee6330af375fd
EkawaD <steo.ederhy@gmail.com>
2022-02-08 16:14:40 +0100

    silly mistake

--- rino.bat
+++ rino.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 
 set install_dir=%USERPROFILE%/.rino
 set list=(xampp symfony python django flask react vue)
-set lib=%install_dir%/batch/lib.bat
+set lib=%install_dir%/lib.bat
 set current=%cd%
 
 set param1=%1
